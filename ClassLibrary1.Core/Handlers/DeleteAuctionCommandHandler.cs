966b4d4efba042f937f1985a76a8ac8cd224816a
Eugene <eugene140823@gmail.com>
2025-07-04 19:19:17 +0300

    fix issues in DeleteAuctionCommandHandler

--- ClassLibrary1.Core/Handlers/DeleteAuctionCommandHandler.cs
+++ ClassLibrary1.Core/Handlers/DeleteAuctionCommandHandler.cs
@@ -1,37 +1,22 @@
 ﻿using ClassLibrary1.Core.Commands;
-using ClassLibrary1.Domain.Data;
-using ClassLibrary1.Domain.Entities;
 using ClassLibrary1.Domain.Interfaces;
 using MediatR;
 
 namespace ClassLibrary1.Core.Handlers
 {
-    public class DeleteAuctionCommandHandler : IRequestHandler<DeleteAuctionCommand, bool>
+    public class DeleteAuctionCommandHandler : IRequest<DeleteAuctionCommand>
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly AuctionDbContext _context;
 
-        public DeleteAuctionCommandHandler(IUnitOfWork unitOfWork, AuctionDbContext context)
+        public DeleteAuctionCommandHandler(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
-            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _unitOfWork = unitOfWork;
         }
 
-        public async Task<bool> Handle(DeleteAuctionCommand request, CancellationToken cancellationToken)
+        public async Task Handle(DeleteAuctionCommand request, CancellationToken cancellationToken)
         {
             var auction = await _unitOfWork.Auctions.GetByIdAsync(request.Id);
-            if (auction == null) return false;
-
-            var deletedAuctionData = new DeletedAuctionData
-            {
-                OriginalId = auction.Id,
-                Name = auction.Name,
-                SourceId = auction.SourceId,
-                DeletedAt = DateTime.UtcNow
-            };
             _unitOfWork.Auctions.Remove(auction);
-            await _unitOfWork.SaveChangesAsync();
-            return true;
         }
     }
 }
